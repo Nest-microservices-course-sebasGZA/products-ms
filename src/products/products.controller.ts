03c06c86b3ecc24dceedf9425c2a1c7200a104eb
sebasGZA <sebassga34@gmail.com>
2025-01-05 01:04:50 -0500

    feat: change message pattern names

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, ParseIntPipe } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PaginationDto } from 'src/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { PaginationDto } from '../common';
 
 @Controller('products')
 export class ProductsController {
